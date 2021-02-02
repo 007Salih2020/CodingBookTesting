3fa4cde3dba743ee487549b4d7e68e907ebe7765
mehmetvural2020 <mehmetvurall@gmail.com>
2021-02-02 09:33:21 +0700

    I added dependencies to Pom.xml file.

---
+++ src/test/java/com/CodingBook/stepdef/MainPageSteps.java
@@ -0,0 +1,19 @@
+package com.CodingBook.stepdef;
+
+public class MainPageSteps {
+//    @io.cucumber.java.en.Given("^User opens CodingBook web page$")
+//    public void userOpensCodingBookWebPage() {
+//    }
+//
+//    @io.cucumber.java.en.When("^User checks if CodingBook logo is visible on the top left corner$")
+//    public void userChecksIfCodingBookLogoIsVisibleOnTheTopLeftCorner() {
+//    }
+//
+//    @io.cucumber.java.en.And("^User clicks CodingBook logo$")
+//    public void userClicksCodingBookLogo() {
+//    }
+//
+//    @io.cucumber.java.en.Then("^Test if user landed CodingBook homepage$")
+//    public void testIfUserLandedCodingBookHomepage() {
+//    }
+}
